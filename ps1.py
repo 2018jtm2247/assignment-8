45d4b34d23937ce8035b1af7113246c2d9b1e675
2018jtm2247 <priyanshu84394@gmail.com>
2018-09-27 18:59:31 +0530

    i hmade changes

--- ps1.py
+++ ps1.py
@@ -14,33 +14,41 @@ for i in range(0,n):
 
 
 print(l)
+print(len(l))
 raw_count=coloum_count=0
 k=1
 i=J=0
 count=0
-#chaeck pattern
-for i in range(0,n):
-	print(i)
-	for j in range(0,m):
-		if l[j] == "S":
-			while l[j-k] == "S" and l[j+k]=="S":
-				raw_count+=1
-				print(raw_count)
-				k+=1
-			k=1	
-			while l[i-k] == "S" and l[i+k]=="S":
-				coloum_count+=1
-				print(coloum_count)
-				k+=1
-			k=1	
-			if raw_count == coloum_count:
-			    count = raw_count + coloum_count + 1          
-                print(count)
-                raw_count = coloum_count = count = 0
 
-            else:
-               raw_count = coloum_count = count = 0    
-               
+#chaeck pattern
+for i in range(0,n):    #raw
+	for j in range(0,m-1): #coloum value in the raw i
+		if "S" in l[j]:	
+		    #cheak horizontal pattern		
+			while (j-k)>=0 and (j+k)<=m and "S" in l[j-k] and "S" in l[j+k] :
+				raw_count+=2
+				print("raw=",raw_count)
+				print(j-k)
+				k+=1
+			k=m-1	
+			print("k=",k)
+	            
+	        #check upper pattern
+			#while l[i-k] in "S" and l[i+k] in "S" and (i-k)>=0 and (i+k)<=n:
+			while (j-k)>=0 and (j+k)<=n and "S" in l[j-k] and "S" in l[j+k] :
+				coloum_count+=2
+				print("coloum=",coloum_count)
+				k=k+k
+			k=1	
+
+			if raw_count == coloum_count and raw_count != 0:
+				count = raw_count + coloum_count + 1
+				print("count=",count)    
+		        
+	#	        raw_count = coloum_count = count = 0
+
+	   		
+			raw_count = coloum_count = count =0
 
 print("end of prog")                
 
