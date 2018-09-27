59ffc5da6566b9d84d78026b8a995b837ccd64ac
2018jtm2247 <priyanshu84394@gmail.com>
2018-09-27 15:12:59 +0530

    First updated file of program 2

--- ps1.py
+++ ps1.py
@@ -1,3 +1,16 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+n,m = list(map(int,input().split()))     #get n raw m coloum
+for i in range(1,n+1):
+	for j in range(1,m+1):
+	#	si = sys.stdin.read(1)
+		si=input()
+		l=list(si)
+		print(l)
+		j+=1
+	print("")	
+	print(si)	
+	i+=1	
+
+
